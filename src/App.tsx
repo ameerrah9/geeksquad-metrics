590e20e97b663d58d1238e10ec037e44c972d8e9
ameerrah9 <mccullers.ghameerah@gmail.com>
2026-04-08 18:38:05 -0400

    Clean up Pages setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,19 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import AutoTech from '@/pages/AutoTech';
 import Overview from '@/pages/Overview';
-import ConsultationAgents from '@/pages/ConsultationAgents';
 import RepairAgents from '@/pages/RepairAgents';
 import AtHomeServices from '@/pages/AtHomeServices';
-import AutoTech from '@/pages/AutoTech';
+import ConsultationAgents from '@/pages/ConsultationAgents';
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<Overview />} />
-        <Route path='/ca' element={<ConsultationAgents />} />
+        <Route path='/auto-tech' element={<AutoTech />} />
         <Route path='/ra' element={<RepairAgents />} />
         <Route path='/at-home' element={<AtHomeServices />} />
-        <Route path='/auto-tech' element={<AutoTech />} />
+        <Route path='/ca' element={<ConsultationAgents />} />
       </Routes>
     </BrowserRouter>
   );
